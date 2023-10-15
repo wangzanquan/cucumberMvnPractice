075cf73347d22d5a5597c687abf734029ae92265
UncleWang <wangzanquan@yahoo.com>
2023-10-15 01:52:10 -0400

    Add report function

--- src/test/java/com/example/TestRunner.java
+++ src/test/java/com/example/TestRunner.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features/",
-        glue = "com.example")
+        glue = "com.example",
+        plugin = {"pretty", "html:target/cucumber/cucumber.html", "json:target/cucumber/cucumber.json"})
 public class TestRunner {
 }
